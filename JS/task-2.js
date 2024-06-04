d0e545241f0c5b90d5164e9e6725770d80ef4461
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-06-04 12:10:06 +0300

    ааа

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,11 +1,26 @@
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// console.log(book);
+
 const book = {
   title: "The Last Kingdom",
   author: "Bernard Cornwell",
   genres: ["historical prose", "adventure"],
-  public: true,
+  isPublic: true,
   rating: 8.38,
 };
 
-console.log(book);
-console.log(book);
-console.log(book);
+const bookTitle = book.title;
+console.log(bookTitle); // "The Last Kingdom"
+
+const bookGenres = book.genres;
+console.log(bookGenres); // ["historical prose", "adventure"]
+
+const bookPrice = book.price;
+console.log(bookPrice); // undefined
