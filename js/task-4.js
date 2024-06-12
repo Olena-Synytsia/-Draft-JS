3ddab5997236a3d7b92f8339e27622bc8a6c2be7
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-06-12 17:30:59 +0300

    add

--- js/task-4.js
+++ js/task-4.js
@@ -37,19 +37,25 @@
 // console.log(filterArray([12, 24, 8, 41, 76], 20)); // повертає [24, 41, 76]
 // console.log(filterArray([10, 2, 8, 41, 76, 100, 14, 32, 41, 0], 22)); //з випадковими, але валідними аргументами, повертає правильне значення [41, 76, 100, 32, 41]
 
-function changeEven(numbers, value) {
-  const newArray = [];
-  numbers.forEach((number) => {
-    number % 2 === 0 ? newArray.push(number + value) : newArray.push(number);
-  });
-  return newArray;
-}
+// function changeEven(numbers, value) {
+//   const newArray = [];
+//   numbers.forEach((number) => {
+//     number % 2 === 0 ? newArray.push(number + value) : newArray.push(number);
+//   });
+//   return newArray;
+// }
 
-changeEven([2, 8, 3, 7, 4, 6], 10); // повертає новий масив `[12, 18, 3, 7, 14, 16]`
-changeEven([17, 24, 68, 31, 42], 100); // повертає новий масив `[17, 124, 168, 31, 142]`
-changeEven([44, 13, 81, 92, 36, 54], 100); // повертає новий масив `[144, 13, 81, 192, 136, 154]`
-changeEven([1, 2, 3, 4, 5], 10); // повертає новий масив `[1, 12, 3, 14, 5]`
+// changeEven([2, 8, 3, 7, 4, 6], 10); // повертає новий масив `[12, 18, 3, 7, 14, 16]`
+// changeEven([17, 24, 68, 31, 42], 100); // повертає новий масив `[17, 124, 168, 31, 142]`
+// changeEven([44, 13, 81, 92, 36, 54], 100); // повертає новий масив `[144, 13, 81, 192, 136, 154]`
+// changeEven([1, 2, 3, 4, 5], 10); // повертає новий масив `[1, 12, 3, 14, 5]`
 
-console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
-console.log(changeEven([17, 24, 68, 31, 42], 100));
-console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+// console.log(changeEven([17, 24, 68, 31, 42], 100));
+// console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+
+const arr = [2, 4, 6];
+const result = arr.filter((el) => el * 3);
+console.log(result);
+
+const booksByAuthor = books.filter((book) => book.author === AUTHOR);
