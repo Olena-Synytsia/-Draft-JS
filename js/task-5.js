b6b8f6f4c84c2ce0cce46b58376bfd1d3f90e9c5
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-06-21 09:21:41 +0300

    add

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,40 @@
-const object = {
-  message: "Hello, World",
-  getMessage() {
-    const message = "Hello, Earth";
-    return this.message;
-  },
-};
-console.log(object.getMessage());
+// const object = {
+//   message: "Hello, World",
+//   getMessage() {
+//     const message = "Hello, Earth";
+//     return this.message;
+//   },
+// };
+// console.log(object.getMessage());
+
+// const person = { name: "Mango" };
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+
+// console.log(sayHi.call(person, 18));
+// // console.log(sayHi.bind(person, 18));
+
+class User {
+  constructor(params) {
+    this.name = params.name;
+    this.email = params.email;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+const mango = new User({
+  name: "Mango",
+  email: "mango@mail.com",
+});
+
+console.log(mango.getEmail()); // “mango@mail.com”
+console.log(mango);
