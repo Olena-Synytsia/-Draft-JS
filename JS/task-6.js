d7e81ae0bb8020bf44577f4f1786546631db5259
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-20 09:33:12 +0300

    2

---
+++ JS/task-6.js
@@ -0,0 +1,14 @@
+// Завдання 6
+// Натиснувши кнопку "Подвоювати", збільшити значення
+// у кожному елементі списку у 2 рази
+// */
+
+//TODO:==============================================
+/*
+Завдання 7
+При натисканні на коло він повинен слідувати за курсором.
+При повторному натисканні він стає в початкове положення.
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
+https://developer.mozilla.org/ru/docs/Web/API/MouseEvent/pageX
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
+*/
